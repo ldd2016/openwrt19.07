35818963fab06c82a0d93b69d3d4d3821dc17739
Ameykyl <37608388+Ameykyl@users.noreply.github.com>
2020-02-16 12:53:00 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -18,7 +18,7 @@ kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget libustream-openssl c
 default-settings luci luci-app-ddns luci-app-sqm luci-app-upnp  luci-app-adbyby-plus luci-app-autoreboot \
 luci-app-filetransfer luci-app-vsftpd ddns-scripts_aliyun luci-app-koolproxyR luci-app-ssr-plus \
 luci-app-arpbind luci-app-vlmcsd luci-app-wifischedule luci-app-wol luci-app-ramfree \
-luci-app-qos-gargoyle libericstools gargoyle-firewall-util libiptbwctl qos-gargoyle kmod-ipt-ndpi kmod-ipt-webmon \
+ libericstools libiptbwctl  kmod-ipt-ndpi kmod-ipt-webmon \
 luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-xlnetacc
 # For nas targets
 DEFAULT_PACKAGES.nas:=fdisk lsblk mdadm automount autosamba luci-app-usb-printer 
